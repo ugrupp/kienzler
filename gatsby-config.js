bd4a9bd51c252e63fe97d2c2816847edf62ee442
Urs Grupp <urs@21sieben.de>
2021-01-04 18:58:03 +0100

    chore: add gatsby default starter and configure mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
---
+++ src/components/marquee.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Marquee = () => {
+  return <marquee>Test</marquee>
+}
+
+export default Marquee
