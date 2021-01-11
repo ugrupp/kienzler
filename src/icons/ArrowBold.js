55999515bf425fb95a111bdd6973643760b1fc8c
Urs Grupp <urs@21sieben.de>
2021-01-11 15:54:51 +0100

    fix: fix invalid SVG attributes

--- src/icons/ArrowBold.js
+++ src/icons/ArrowBold.js
@@ -8,15 +8,15 @@ const ArrowBoldIcon = props => {
         d="M22.75 22.75h-22v-22h22z"
         fill="none"
         stroke="currentColor"
-        stroke-miterlimit="10"
-        stroke-width="1.5"
+        strokeMiterlimit="10"
+        strokeWidth="1.5"
       />
       <path fill="currentColor" d="M14.77 8.9l2.82 2.82-2.82 2.83V8.9z" />
       <path
         fill="none"
         stroke="currentColor"
-        stroke-miterlimit="10"
-        stroke-width="1.5"
+        strokeMiterlimit="10"
+        strokeWidth="1.5"
         d="M5.88 11.73h9.7"
       />
     </Icon>
