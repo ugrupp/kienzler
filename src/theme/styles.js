5dd964c82e16ca3edd1e184dd4109cf9ca00ea31
Urs Grupp <urs@21sieben.de>
2021-02-09 22:46:40 +0100

    chore: add selection styling

--- src/theme/styles.js
+++ src/theme/styles.js
@@ -4,12 +4,19 @@ export default {
       // scrollBehavior: "smooth",
       textSizeAdjust: "100%",
     },
+
     body: {
       fontSize: "1rem",
       color: "gray.500",
     },
+
     "*, *::before, &::after": {
       wordWrap: null,
+    },
+
+    "::selection": {
+      bgColor: "orange.500",
+      color: "white",
     },
   },
 }
