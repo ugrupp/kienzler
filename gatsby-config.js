d31a53df5bbd967f5bf130a4b5fc7b64bf3cd143
Urs Grupp <urs@21sieben.de>
2021-03-30 15:25:29 +0200

    feat: update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,12 +58,12 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Kienzler Betontechnik`,
-        short_name: `kienzler`,
+        short_name: `Kienzler`,
         start_url: `/`,
         background_color: `#676767`,
         theme_color: `#EA6A1F`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
